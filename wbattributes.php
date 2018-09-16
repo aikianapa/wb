d2afac5a5a43ebec5690c311debf3ead1e03178b
Unknown <oleg_frolov@mail.ru>
2018-09-16 20:42:46 +0300

    wbattributes

--- wbattributes.php
+++ wbattributes.php
@@ -1,10 +1,13 @@
 <?php
     $_attributes=$this->attributes();
+    $_ENV["attributes"]=array();
     if ($_attributes) {
 	$attrs=new IteratorIterator($_attributes);
 	foreach ($attrs as $attr) {
 		$tmp=$attr->name;
-		if (strpos($tmp,"ata-wb-")) {$tmp=str_replace("data-wb-","",$tmp); $$tmp=$attr->value()."";}
+		if (strpos($tmp,"ata-wb-")) {$tmp=str_replace("data-wb-","",$tmp); $_ENV["attributes"][$tmp]=$$tmp=$attr->value()."";} else {
+			$_ENV["attributes"][$attr->name]=$attr->value();
+		}
 	}; unset($attrs);
     }
 ?>
