85494b3dcf187b2b97a6bef6a5c8d1011df4f1b3
Unknown <oleg_frolov@mail.ru>
2019-06-06 17:52:44 +0300

    weprocessor

--- wb_set_values_str.php
+++ wb_set_values_str.php
@@ -5,6 +5,8 @@ include_once(__DIR__."/weprocessor/weprocessor.php");
 function wbSetValuesStr($tag = "",$Item = array(), $limit = 2, $vars = null)
 {
 	if (is_object($tag)) $tag = $tag->outerHtml();
+	if (!strpos($tag,"}}")) return $tag;
+
     if (true) {
         $processor = new WEProcessor($Item);
         return $processor->substitute($tag);
