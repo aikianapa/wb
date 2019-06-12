389d41bb4f6cef6ccfed741a4bdb54b96d4b6148
Unknown <oleg_frolov@mail.ru>
2019-06-12 08:26:17 +0300

    update

--- tags/foreach/foreach.php
+++ tags/foreach/foreach.php
@@ -255,7 +255,7 @@ class tagForeach extends kiNode  {
                             $stp++;
                         }
                     } else { // иначе строим строку
-                        $inner.=wbClearValues($text->outerHtml());
+                        $inner.=$text->outerHtml();
                     }
                 }
 
